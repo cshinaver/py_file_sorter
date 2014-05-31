0c34a2473a4bb420f4936d1824cd47cabffbc8b5
Charles Shinaver <charlesshinaver@Charless-MacBook-Pro.local>
2014-05-30 22:51:12 -0400

    Added loop that would go through each file in sort path
    Former-commit-id: 7fc0fb7c9bc39a96af06383cab5ab3b373014d92

--- file_sorter.py
+++ file_sorter.py
@@ -1,5 +1,4 @@
-"""Sorts downloads folder"""
-#!/usr/bin/python
+# """Sorts downloads folder"""
 import os
 from shutil import move
 DOWNLOAD_DIRECTORY_PATH = os.path.dirname(__file__)
@@ -10,16 +9,20 @@ def primary_sort(file_path):
     """First pass for sorting.
     Sorts file that might go to plex (video files) into a separate folder. Might just call secondary function for that file.
     """
+    # Get file extension
     file_name = os.path.basename(file_path)
     file_extension = os.path.splitext(file_path)
     file_extension = file_extension[1]
 
     # If file is a movie, move to sorted directory (Possibly Rename?)
     if file_extension in ('.mpeg', '.mpg', '.avi', '.mov', '.qt', '.wmv', '.mp4', '.ogm', '.mkv'):
-        #TODO 1. Add check if sorted_files directory exists
+        # TODO 1. Add check if sorted_files directory exists
         DESTINATION_PATH = os.path.join(DOWNLOAD_DIRECTORY_PATH, 'sorted_files')
         DESTINATION_PATH = os.path.join(DESTINATION_PATH, file_name)
         move(file_path, DESTINATION_PATH)
+	print "%s was moved to %s" % (file_name, DESTINATION_PATH)
 
 
-primary_sort(my_file_path)
+for sort_file in os.listdir(SORTING_DIRECTORY_PATH):
+    sort_file_path = os.path.join(SORTING_DIRECTORY_PATH, sort_file)
+    primary_sort(sort_file_path)
