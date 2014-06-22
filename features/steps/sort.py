6332bd4bba72573ea1866304a32cd445eeb5b3ec
Charles Shinaver <bshinaver@gmail.com>
2014-06-22 00:38:12 -0400

    Moved features out of filesort dir
    Former-commit-id: ba61038379b33c4d45c23ed3ce27a26ecf31a5e2

--- features/steps/sort.py
+++ features/steps/sort.py
@@ -2,32 +2,35 @@
 Feature: Download_Sorter (Natural Language)
     In order to have a clean and sorted downloads folder,
     As the admin
-    I want the downloads to be moved based on type to 
+    I want the downloads to be moved based on type to
         the set destinations. In my case, the media server
 
 
     Scenario: Movie
-        Given a sort path 
+        Given a sort path
         And a movie path
         When the file is a movie
         Then the file should be moved to the Movie Folder
 """
-from behave import *
-from 
+from behave import *  # noqa
+from filesort.file_sorter import FileSort
+
 
 @given('a sort path')
-def step_impl(context):
-    assert 2 ==3
+def step_a_sort_path(context):
+    assert 2 == 3
+
 
 @given('a movie path')
-def step_impl(context):
+def step_a_movie_path(context):
     assert False
+
 
 @when('the file is a movie')
-def step_impl(context):
+def step_file_is_movie(context):
     assert False
+
 
 @then('the file should be moved to the Movie Folder')
-def step_impl(context):
+def step_file_moved(context):
     assert False
-
