0abab8500c5fab400e49dff1c70d121c450017cd
Charles Shinaver <charlesshinaver@Charless-MacBook-Pro.local>
2014-07-04 23:56:17 -0400

    Made methods into classes
    Former-commit-id: 70af43c66a819c57c90dc17f7c157d2f308105fb

--- filesort/file_sorter.py
+++ filesort/file_sorter.py
@@ -22,80 +22,78 @@ class FileSort:
     tv_show_dir = ''
     lynda_dir = ''
 
-#TODO Write function that takes args like sort_downloads,
-# sort_TV_Shows, sort_Movies
+    #TODO Write function that takes args like sort_downloads,
+    # sort_TV_Shows, sort_Movies
+    def primary_sort(self, file_path):
+        #TODO Add sorting for Other things that aren't videos
+        """
+        Sorts given filepath into Movies and Learning Videos
+        """
+        # Get file extension
+        file_name = os.path.basename(file_path)
+        file_extension = os.path.splitext(file_path)
+        file_extension = file_extension[1]
 
+        # Movie Selection and Sorting
+        if file_extension in ('.mpeg',
+                              '.mpg',
+                              '.avi',
+                              '.mov',
+                              '.qt',
+                              '.wmv',
+                              '.mp4',
+                              '.ogm',
+                              '.mkv'
+                              ):
+            # TODO 1. Possibly add something to differentiate between
+            # TV Shows and
+            # Movies (if not Lynda.com files)
+            if file_name.find("Lynda.com") != -1:
+                self.directory_exists(LEARNING_VIDEOS_DESTINATION_PATH)
+                move(file_path, LEARNING_VIDEOS_DESTINATION_PATH)
+                print('{0} was moved to {1}'.format(
+                    file_name,
+                    LEARNING_VIDEOS_DESTINATION_PATH,
+                    ))
+            else:
+                # TODO 2. Also, remove directory after transfer
 
-def primary_sort(file_path, movie_dir_path):
-    #TODO Add sorting for Other things that aren't videos
-    """
-    Sorts given filepath into Movies and Learning Videos
-    """
-    # Get file extension
-    file_name = os.path.basename(file_path)
-    file_extension = os.path.splitext(file_path)
-    file_extension = file_extension[1]
+                self.directory_exists(self.movie_dir)
+                move(file_path, self.movie_dir)
+                #remove_dirtree(file_path)
+                print('{0} was moved to {1}'.format(
+                    file_name,
+                    self.movie_dir,
+                    ))
 
-    # Movie Selection and Sorting
-    if file_extension in ('.mpeg',
-                          '.mpg',
-                          '.avi',
-                          '.mov',
-                          '.qt',
-                          '.wmv',
-                          '.mp4',
-                          '.ogm',
-                          '.mkv'
-                          ):
-        # TODO 1. Possibly add something to differentiate between TV Shows and
-        # Movies (if not Lynda.com files)
-        if file_name.find("Lynda.com") != -1:
-            directory_exists(LEARNING_VIDEOS_DESTINATION_PATH)
-            move(file_path, LEARNING_VIDEOS_DESTINATION_PATH)
-            print('{0} was moved to {1}'.format(
-                file_name,
-                LEARNING_VIDEOS_DESTINATION_PATH,
+    def directory_exists(self, directory_path):
+        """Checks if directory exists. Creates it if test fails"""
+        if not os.path.isdir(directory_path):
+            os.makedirs(directory_path)
+            print('{0} did not exist, so it was created'.format(
+                directory_path,
                 ))
-        else:
-            # TODO 2. Also, remove directory after transfer
 
-            directory_exists(movie_dir_path)
-            move(file_path, movie_dir_path)
-            remove_dirtree(file_path)
-            print('{0} was moved to {1}'.format(
-                file_name,
-                movie_dir_path,
-                ))
+    def main_sorter(self):
+        """Main function"""
+        print("Beginning file sort...")
+        # Recursively sort SORTING_DIRECTORY_PATH
+        for sort_dir, dirs, files in os.walk(self.sort_dir):
+            print("Checking " + str(sort_dir))
+            for file in files:
+                file_path = os.path.join(self.sort_dir, file)
+                self.primary_sort(file_path)
+        print("Sorting Complete.")
 
+    def remove_dirtree(file_path):
+        #TODO Finish this function
+        """Removes parent dirtree for given file_path"""
+        len_sort_dir = len(SORTING_DIRECTORY_PATH)
+        print(file_path)
+        print(file_path[len_sort_dir:])
 
-def directory_exists(directory_path):
-    """Checks if directory exists. Creates it if test fails"""
-    if not os.path.isdir(directory_path):
-        os.makedirs(directory_path)
-        print('{0} did not exist, so it was created'.format(directory_path))
-
-
-def main_sorter(sort_dir_path, movie_dir_path):
-    """Main function"""
-    print("Beginning file sort...")
-    # Recursively sort SORTING_DIRECTORY_PATH
-    for sort_dir, dirs, files in os.walk(sort_dir_path):
-        print("Checking " + str(sort_dir))
-        for file in files:
-            file_path = os.path.join(sort_dir_path, file)
-            primary_sort(file_path, movie_dir_path)
-    print("Sorting Complete.")
-
-
-def remove_dirtree(file_path):
-    #TODO Finish this function
-    """Removes parent dirtree for given file_path"""
-    len_sort_dir = len(SORTING_DIRECTORY_PATH)
-    print(file_path)
-    print(file_path[len_sort_dir:])
-
-# my_file_path = os.path.join(SORTING_DIRECTORY_PATH, 'mah_dir')
-# my_file_path = os.path.join(my_file_path, 'show.mkv')
-# remove_dirtree(my_file_path)
-# if __name__ == "__main__":
-#     main()
+    # my_file_path = os.path.join(SORTING_DIRECTORY_PATH, 'mah_dir')
+    # my_file_path = os.path.join(my_file_path, 'show.mkv')
+    # remove_dirtree(my_file_path)
+    # if __name__ == "__main__":
+    #     main()
